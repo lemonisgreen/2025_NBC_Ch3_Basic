fede7e0fb90bf53c851f938a95b43172d3c0bd9d
LEMONY <jin.lee1028@gmail.com>
2025-04-06 15:56:54 +0900

    git ignore 생성

---
+++ StarterTeam2Github/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarterTeam2Github
+//
+//  Created by JIN LEE on 4/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
