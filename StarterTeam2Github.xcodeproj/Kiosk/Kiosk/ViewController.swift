5c22c5a90b25d5a8e727996a30a4199af9877b28
LEMONY <138443851+lemonisgreen@users.noreply.github.com>
2025-04-07 20:12:33 +0900

    Add files via upload

---
+++ StarterTeam2Github.xcodeproj/Kiosk/Kiosk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kiosk
+//
+//  Created by JIN LEE on 4/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
