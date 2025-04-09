4e56151ed7b4941f1db8742252a6f6070a266e45
NH <mnh4140@gmail.com>
2025-04-09 11:50:37 +0900

    [Feat] #6 - ViewController 병합
    - 따로 개발을 하고있던 ViewController를 하나로 병합하였습니다.

--- Kiosk/CollectionView/CollectionViewDelegate.swift
+++ Kiosk/CollectionView/CollectionViewDelegate.swift
@@ -7,53 +7,57 @@
 
 import UIKit
 
-extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    
-    // 셀의 수
+// MARK: - 컬렉션 뷰 데이터소스
+extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
-        switch SegmentState(rawValue: state) {
-        case .jeontongjoo:
-            return jeontongjooList.count
-        case .wine:
-            return wineList.count
-        case .sake:
-            return sakeList.count
-        default:
-            return 0
+        if collectionView == cartCollectionView { // 컬렉션 뷰가 장바구니일때
+            return 0 // 아직 구현 안되서 초기값 설정
+        } else {
+            switch SegmentState(rawValue: state) {
+            case .jeontongjoo:
+                return jeontongjooList.count
+            case .wine:
+                return wineList.count
+            case .sake:
+                return sakeList.count
+            default:
+                return 0
+            }
         }
     }
     
     // 셀에 표시될 아이템(CollectionViewCell에서 configure 메서드에 UI에 띄우는 로직을 구현해둠)
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.id, for: indexPath) as? CollectionViewCell else { return UICollectionViewCell() }
-        
-        switch SegmentState(rawValue: state) {
-        case .jeontongjoo:
-            cell.configure()
-            cell.setJeontongjoo(jeontongjooList[indexPath.item])
-        case .wine:
-            cell.configure()
-            cell.setWine(wineList[indexPath.item])
-        case .sake:
-            cell.configure()
-            cell.setSake(sakeList[indexPath.item])
-        default:
-            print("Error")
+        if collectionView == cartCollectionView{ // 컬렉션뷰가 장바구니일 때
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CartCell", for: indexPath) as? CartCollectionViewCell else {
+                return UICollectionViewCell()
+            }
+            
+            return cell // 아직 구현 안됨
+        } else {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.id, for: indexPath) as? CollectionViewCell else { return UICollectionViewCell() }
+            
+            switch SegmentState(rawValue: state) {
+            case .jeontongjoo:
+                cell.configure()
+                cell.setJeontongjoo(jeontongjooList[indexPath.item])
+            case .wine:
+                cell.configure()
+                cell.setWine(wineList[indexPath.item])
+            case .sake:
+                cell.configure()
+                cell.setSake(sakeList[indexPath.item])
+            default:
+                print("Error")
+            }
+            
+            return cell
         }
-        
-        return cell
     }
-    
-    // 셀의 크기 설정
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        // 하나씩 밑으로 늘어뜨린 리스트형식으로 출력되게 할 것이기 때문에 width를 cellectionView와 같게 함
-        let width = collectionView.bounds.width
-        return CGSize(width: width, height: 120)
-    }
-    
+}
+
+// MARK: - 컬렉션 뷰 델리게이트
+extension ViewController: UICollectionViewDelegate {
     // 선택된 item을 호출할 메서드
     func collectionView(_ collectionView: UICollectionView, didUnhighlightItemAt indexPath: IndexPath) {
         switch SegmentState(rawValue: state) {
@@ -71,3 +75,15 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, 
         }
     }
 }
+
+// MARK: - 컬렉션 뷰 플로우 레이아웃
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    // 셀의 크기 설정
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        // 하나씩 밑으로 늘어뜨린 리스트형식으로 출력되게 할 것이기 때문에 width를 cellectionView와 같게 함
+        let width = collectionView.bounds.width
+        return CGSize(width: width, height: 120)
+    }
+}
+
--- Kiosk/ViewController.swift
+++ Kiosk/ViewController.swift
@@ -8,6 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: -  >>>>>>>>>>>>>>>>> 명노훈 장바구니 <<<<<<<<<<<<<<<<<<<<<<<<<
+    // MARK: - Properties
+    let cartlabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "장바구니"
+        label.textAlignment = .left
+        label.textColor = .font
+        label.font = .boldSystemFont(ofSize: 20)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    // MARK: - 컬렉션 뷰 인스턴스 정의
+    lazy var cartCollectionView: UICollectionView = {
+        // 1. 셀의 배치를 정해주는 레이아웃 객체 생성
+        let layout = UICollectionViewFlowLayout()
+        
+        layout.scrollDirection = .vertical // 세로 방향 스크롤
+        layout.minimumLineSpacing = 0 // 셀 사이의 간격 (줄 간격)
+        layout.itemSize = CGSize(width: 351, height: 40) // 셀 크기 설정
+        
+        // 2. 컬렉션 뷰 생성
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        collectionView.backgroundColor = .white // 배경색 지정
+        
+        // 3. 델리게이트, 데이터소스 연결
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        collectionView.layer.cornerRadius = 20 // 둥굴게 설정
+        collectionView.translatesAutoresizingMaskIntoConstraints = false // 오토레이아웃 설정
+        
+        // 4. 커스텀 셀 등록
+        collectionView.register(CartCollectionViewCell.self, forCellWithReuseIdentifier: "CartCell")
+        return collectionView
+    }()
+    
+    // MARK: - >>>>>>>>>>>>>>>>> 최규현 메뉴 화면 <<<<<<<<<<<<<<<<<<<<<<<<<
     
     // collectionView 생성 및 Data를 담아둔 구조체의 인스턴스 생성
     var collectionView: CollectionView!
@@ -28,8 +69,11 @@ class ViewController: UIViewController {
         configure()
         registerCollectionView()
         delegatesOn()
+        
+        // 장바구니 관련 함수
+        setUI()
+        setLabel()
+        setCollectionView()
     }
-
-
 }
 
