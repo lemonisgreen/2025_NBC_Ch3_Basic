201cf4857998cac265f6f6ae5db50200948eeb66
ghnn-n <cgh6470@gmail.com>
2025-04-09 17:03:26 +0900

    [Design] #11 - Button UI 구현

---
+++ Kiosk/Button/ButtonAction.swift
@@ -0,0 +1,19 @@
+//
+//  ButtonAction.swift
+//  Kiosk
+//
+//  Created by 최규현 on 4/9/25.
+//
+
+import UIKit
+
+extension ViewController {
+    
+    @objc func orderButtonTapped() {
+        
+    }
+    
+    @objc func cancelButtonTapped() {
+        
+    }
+}
--- Kiosk/CollectionView/ViewControllerExtension.swift
+++ Kiosk/CollectionView/ViewControllerExtension.swift
@@ -21,19 +21,49 @@ extension ViewController {
         let collectionView = CollectionView(frame: .zero, collectionViewLayout: collectionViewLayer)
         collectionView.backgroundColor = .white
         collectionView.layer.cornerRadius = 20
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
         
-        view.addSubview(collectionView)
+        self.collectionView = collectionView
+        
+        cancelButton.setTitle("주문 취소", for: .normal)
+        cancelButton.setBackgroundColor(.sub2, for: .normal)
+        cancelButton.addTarget(self, action: #selector(cancelButtonTapped), for: .touchUpInside)
+        
+        orderButton.setTitle("주문 하기", for: .normal)
+        orderButton.setBackgroundColor(.sub1, for: .normal)
+        orderButton.addTarget(self, action: #selector(orderButtonTapped), for: .touchUpInside)
+        
+        [orderButton, cancelButton]
+            .forEach {
+                $0.titleLabel?.font = .boldSystemFont(ofSize: 20)
+                $0.setTitleColor(.main, for: .normal)
+                $0.layer.cornerRadius = 21.5
+                $0.layer.masksToBounds = true
+            }
+        
+        [collectionView, orderButton, cancelButton]
+            .forEach {
+                $0.translatesAutoresizingMaskIntoConstraints = false
+                view.addSubview($0)
+            }
         
         // 오토레이아웃 설정
         NSLayoutConstraint.activate([
             collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 160),
             collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12),
             collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12),
-            collectionView.heightAnchor.constraint(equalToConstant: 310)
+            collectionView.heightAnchor.constraint(equalToConstant: 310),
+            
+            cancelButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40),
+            cancelButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12),
+            cancelButton.widthAnchor.constraint(equalToConstant: 168),
+            cancelButton.heightAnchor.constraint(equalToConstant: 43),
+            
+            orderButton.bottomAnchor.constraint(equalTo: cancelButton.bottomAnchor),
+            orderButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12),
+            orderButton.widthAnchor.constraint(equalToConstant: 168),
+            orderButton.heightAnchor.constraint(equalToConstant: 43)
         ])
         
-        self.collectionView = collectionView
     }
     
     // identifier 설정(CollectionViewCell에서 만들었던 id 사용)
